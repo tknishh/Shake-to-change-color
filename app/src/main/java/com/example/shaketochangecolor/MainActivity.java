44872c9dde64b1262de7c950b6433d4c84f9e705
vaniseth <74897823+vaniseth@users.noreply.github.com>
2022-05-30 07:44:16 +0530

    updated main file

--- app/src/main/java/com/example/shaketochangecolor/MainActivity.java
+++ app/src/main/java/com/example/shaketochangecolor/MainActivity.java
@@ -1,11 +1,34 @@
 package com.example.shaketochangecolor;
 
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-@Override
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private boolean isColor = false;
+    private View view;
+    private long lastUpdate;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        view = findViewById(R.id.textView);
+        view.setBackgroundColor(Color.CYAN);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lastUpdate = System.currentTimeMillis();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            getAccelerometer(sensorEvent);
+        }
+
     }
