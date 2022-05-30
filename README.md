eb4aaa48a855811bbd55912de57286fb14e2c912
Tanish Khandelwal <76485783+tknishh@users.noreply.github.com>
2022-05-30 07:37:59 +0530

    add files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shake-to-Change-Color
+
+In this repository I have done some work with shake detection in android application
---
+++ app/src/main/java/com/example/shaketochangecolor/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.shaketochangecolor;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
